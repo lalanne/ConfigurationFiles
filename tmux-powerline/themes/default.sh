56e7b415bfebe89c7bfb2864969f164edc4d925d
Christian Lalanne <christian.lalanne@ichec.ie>
2015-06-05 10:57:12 +0100

    ADD:battery segment

--- tmux-powerline/themes/default.sh
+++ tmux-powerline/themes/default.sh
@@ -46,7 +46,7 @@ TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 #"cpu 240 136" \
 #"load 237 167" \
 #"tmux_mem_cpu_load 234 136" \
-#"battery 137 127" \
+         "battery 137 127" \
 # "weather 37 255" \
 #"rainbarf 0 0" \
 #"xkb_layout 125 117" \
