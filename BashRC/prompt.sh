10d98571c6112c1c262526c99f0f4a34c86c0b1f
Christian <lalanne@gmail.com>
2015-06-05 01:09:34 +0100

    ADD: allow tmux-powerline to show PWD

--- BashRC/prompt.sh
+++ BashRC/prompt.sh
@@ -4,3 +4,6 @@ green="\e[0;32m"
 blue="\e[0;34m"
 reset="\e[0m"
 PS1='\['${red}'\]\u\['${reset}'\]@\['${green}'\]\h\['${reset}'\]:\['${blue}'\]\w\['${reset}'\]\$ '
+#For tmux to figure out the path
+PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
+
