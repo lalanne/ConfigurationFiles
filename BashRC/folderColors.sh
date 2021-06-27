74f1b5c9bcc3b91a842897fbee8d680d2a1d45ac
Christian Lalanne <lalanne@gmail.com>
2021-06-26 21:44:26 -0400

    DEL: not using bash anymore, using zsh

--- BashRC/folderColors.sh
+++
@@ -1,5 +0,0 @@
-#colors
-export CLICOLOR=1;
-export LSCOLORS=exfxcxdxbxegedabagacad
-# Tell grep to highlight matches
-export GREP_OPTIONS='--color=auto'
--- BashRC/prompt.sh
+++
@@ -1,9 +0,0 @@
-
-red="\e[0;31m"
-green="\e[0;32m"
-blue="\e[0;34m"
-reset="\e[0m"
-PS1='\['${red}'\]\u\['${reset}'\]@\['${green}'\]\h\['${reset}'\]:\['${blue}'\]\w\['${reset}'\]\$ '
-#For tmux to figure out the path
-PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
-
